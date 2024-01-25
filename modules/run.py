9d45d378cdf8938b433136c09c29c530c9d620de
ericbretz <ebretz2@uic.edu>
2024-01-25 16:46:53 -0600

    2.0.1

--- modules/run.py
+++ modules/run.py
@@ -112,20 +112,22 @@ class MAIN:
         
         with open(self.ASSEMBLY, 'r') as assembly_f:
             for line in assembly_f:
-                if line.strip('>').strip('\n') in goodlst:
-                    goodseq = [line]
-                    line = next(assembly_f)
-                    while True:
-                        if line.startswith('>'):
-                            with open(self.GOODFA, 'a') as good_f:
-                                good_f.write(''.join(goodseq))
-                            goodseq = []
-                            break
-                        try:
-                            line = next(assembly_f)
-                            goodseq.append(line)
-                        except:
-                            break
+                if line.startswith('>'):
+                    if line.strip('>').strip('\n') in goodlst:
+                        goodseq = [line]
+                        line = next(assembly_f)
+                        while True:
+                            if line.startswith('>'):
+                                with open(self.GOODFA, 'a') as good_f:
+                                    good_f.write(''.join(goodseq))
+                                goodseq = []
+                                break
+                            try:
+                                line = next(assembly_f)
+                                if not line.startswith('>'):
+                                    goodseq.append(line)
+                            except:
+                                break
                              
             with open(self.GOODFA, 'a') as good_f:
                 good_f.write(''.join(goodseq))
