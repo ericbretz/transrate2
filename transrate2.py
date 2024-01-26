a8e739fba18ad8bf4289a6bb3fb56bec5b1519e1
ericbretz <ebretz2@uic.edu>
2024-01-26 12:49:08 -0600

    2.0.3

--- transrate2.py
+++ transrate2.py
@@ -137,7 +137,8 @@ if __name__ == '__main__':
                 bottombar = f'{self.color}  └{"─" * 74}┘\033[m'
                 print(topbar)
                 if self.assemblytotal > 1:
-                    print(f'{self.color}  │\033[m Assembly #: {self.color}{self.assemblycount + 1}/{self.assemblytotal}{self.color}{" " * 57}│\033[m')
+                    y = 74 - len(str(self.assemblycount + 1)) - len(str(self.assemblytotal)) - 15
+                    print(f'{self.color}  │\033[m Assembly #: {self.assemblycount + 1}/{self.assemblytotal}{self.color}{" " * y}│\033[m')
                 for k,v in args.__dict__.items():
                     if v:
                         if k == 'assembly':
